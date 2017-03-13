1855a2afcdc7cd708f9cb6e4b94a1d9698cc45e9
Elias Mimouni <emimouni10@hotmail.com>
2017-03-13 06:23:05 -0400

    Update teams.java

--- src/com/company/teams.java
+++ src/com/company/teams.java
@@ -1,20 +1,34 @@
 package com.company;
 
 /**
- * Created by em343 on 3/8/17.
+ * Created by emimo on 3/11/2017.
  */
 public class teams {
-    public String name;
-    public double winPercent;
-    public teams(String n, double w){
-        name = n;
-        winPercent = w;
-    }
+        public String name;
+        public double winPercent;
+        public double strengthOfSchedule;
+        public double ppt;
+        public double dpt;
+        public teams(String n, double w, double d, double p, double de){
+            name = n;
+            winPercent = w;
+            strengthOfSchedule = d;
+            ppt=p;
+            dpt = de;
+        }
 
-    public String getName(){
-        return this.name;
-    }
-    public double getWinPercentage(){
-        return this.winPercent;
+        public String getName(){
+            return this.name;
+        }
+        public double getWinPercentage(){
+            return this.winPercent;
+        }
+        public double getStrengthOfSchedule(){
+            return this.strengthOfSchedule;
+        }
+    public double getPpt(){
+        return this.ppt;
+    }    public double getDpt(){
+        return this.dpt;
     }
 }
